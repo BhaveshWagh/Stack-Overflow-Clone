445c0827038df370035e3a64e9eb0370232f738d
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-10 16:49:53 +0530

    left and right sidebar completed

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
+import LeftSidebar from '../../components/LeftSidebar/LeftSidebar'
+import HomeMainbar from '../../components/HomeMainbar/HomeMainbar'
+import RightSidebar from '../../components/RightSidebar/RightSidebar'
+import '../../App.css'
 
 const Home = () => {
   return (
-    <div>
-      Home
+    <div className='home-container-1'>
+      <LeftSidebar/>
+      <div className='home-container-2'>
+        <HomeMainbar/>
+        <RightSidebar/>
+      </div>
     </div>
   )
 }
