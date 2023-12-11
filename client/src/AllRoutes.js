70a83b7c6a88a2424aaa4e44d165c2a9455f518a
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-11 15:30:03 +0530

    display questions page completed

--- client/src/AllRoutes.js
+++ client/src/AllRoutes.js
@@ -4,17 +4,18 @@ import { Routes, Route } from "react-router-dom";
 
 import Home from "./Pages/Home/Home";
 import Auth from "./Pages/Auth/Auth";
-import Questions  from "./Pages/Questions/Questions";
+import Questions from "./Pages/Questions/Questions";
 import AskQuestion from "./Pages/AskQuestion/AskQuestion";
+import DisplayQuestion from "./Pages/Questions/DisplayQuestion";
 
 const AllRoutes = () => {
   return (
     <Routes>
-      <Route  path="/" element={<Home/>}/>
-      <Route  path="/Auth" element={<Auth/>}/>
-      <Route path="/Questions" element={<Questions/>}/>
-      <Route path="/AskQuestion" element={<AskQuestion/>}/>
-   
+      <Route path="/" element={<Home />} />
+      <Route path="/Auth" element={<Auth />} />
+      <Route path="/Questions" element={<Questions />} />
+      <Route path="/AskQuestion" element={<AskQuestion />} />
+      <Route path="/Questions/:id" element={<DisplayQuestion />} />
     </Routes>
   );
 };
--- client/src/components/HomeMainbar/HomeMainbar.jsx
+++ client/src/components/HomeMainbar/HomeMainbar.jsx
@@ -8,33 +8,57 @@ const HomeMainbar = () => {
   var questionsList = [
     {
       id: 1,
-      votes: 3,
+      upVotes: 3,
+      downVotes:1,
       noOfAnswers: 2,
       questionTitle: "What is a function ?",
       questionBody: "It meant to be",
       questionTags: ["java", "node js", "react js", "mongodb", "javascript"],
       userPosted: "Monu",
       askedOn: "Nov 27 2000",
+      useId:1,
+      answer:[{
+        answerBody:"Answer",
+        userAnswered:'Kumar Sanu',
+        answeredOn:"Nov 30 2000",
+        userId:2,
+      }]
     },
     {
       id: 2,
-      votes: 2,
+      upVotes: 345,
+      downVotes: 23,
       noOfAnswers: 23,
       questionTitle: "What is rendering in reactjs ?",
       questionBody: "It meant to be",
       questionTags: ["java", "node js", "react js", "mongodb", "javascript"],
       userPosted: "Sonu",
       askedOn: "Jan 15 2020",
+      useId:1,
+      answer:[{
+        answerBody:"Answer",
+        userAnswered:'Ajay Kumar',
+        answeredOn:"Jan 17 2020",
+        userId:2,
+      }]
     },
     {
       id: 3,
-      votes: 111,
+      upVotes: 123,
+      downVotes:10,
       noOfAnswers: 50,
       questionTitle: "What is a API's ?",
       questionBody: "It meant to be",
       questionTags: ["java", "node js", "react js", "mongodb", "javascript"],
       userPosted: "Tinu",
       askedOn: "Feb 1 2018",
+      useId:1,
+      answer:[{
+        answerBody:"Answer",
+        userAnswered:'Kumar Sanu',
+        answeredOn:"Jan 17 2020",
+        userId:2,
+      }]
     },
   ];
 
