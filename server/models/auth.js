f185706a311436cacd19dcd175359e8f6ac1e867
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-12 18:16:08 +0530

    authentication with mongodb is completed

---
+++ server/models/auth.js
@@ -0,0 +1,15 @@
+// Models nothing but a users schema of database
+
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true },
+  password: { type: String, required: true },
+  about: { type: String },
+  tags: { type: [String] },
+  joinedOn: { type: Date, default:Date.now },
+});
+
+
+export default mongoose.model("User",userSchema)
