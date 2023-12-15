1f8274e6be9b2cdfb32e9552f809edc785bcc352
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-15 17:57:07 +0530

    feat : Token Based Authentication done

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,22 +1,38 @@
 import React, { useEffect } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import search from "../../assets/search-solid.svg";
 import Avatar from "../../components/Avatar/Avatar";
 // import Button from "../../components/Button/Button";
 import { useSelector, useDispatch } from "react-redux";
 
+import { jwtDecode } from "jwt-decode";
+
 import "./Navbar.css";
 import { setCurrentUser } from "../../actions/currentUser";
 const Navbar = () => {
   const dispatch = useDispatch();
+  const navigate = useNavigate();
   var User = useSelector((state) => state.currentUserReducer);
 
+  const handleLogout = () => {
+    dispatch({ type: "LOGOUT" });
+    navigate("/");
+    dispatch(setCurrentUser(null));
+  };
+
   useEffect(() => {
+    const token = User?.token;
+    if (token) {
+      const decodedToken = jwtDecode(token);
+      // console.log(token)
+      if (decodedToken.exp * 1000 < new Date().getTime()) {
+        handleLogout();
+      }
+    }
     dispatch(setCurrentUser(JSON.parse(localStorage.getItem("Profile"))));
   }, [dispatch]);
 
-  
   return (
     <nav className="main-nav">
       <div className="navbar">
@@ -58,11 +74,12 @@ const Navbar = () => {
                 {User.result.name.charAt(0).toUpperCase()}
               </Link>
             </Avatar>
-            <button className="nav-item nav-links">Log Out</button>
+            <button className="nav-item nav-links" onClick={handleLogout}>
+              Log Out
+            </button>
           </>
         )}
         {/* </div> */}
-        
       </div>
     </nav>
   );
--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -4,7 +4,9 @@ const authReducer = (state = { data: null }, action) => {
       // we can store in 3 ways store as a cookie, session & localStorage
       localStorage.setItem("Profile", JSON.stringify({ ...action?.data }));
       return { ...state, data: action?.data };
-
+    case "LOGOUT":
+      localStorage.clear();
+      return { ...state, data: null };
     default:
       return state;
   }
