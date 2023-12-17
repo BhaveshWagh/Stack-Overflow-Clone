7a780f1a7f7954c454f2b3ca8cad05996e6d7927
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-12-17 20:10:14 +0530

    all setup done for hosting app

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -18,7 +18,7 @@ export const signup = async (req, res) => {
       email,
       password: hashedPassword,
     });
-    const token = jwt.sign({ email: newUser.email, id: newUser._id }, "test", {
+    const token = jwt.sign({ email: newUser.email, id: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
     // status 200 : success
@@ -40,7 +40,7 @@ export const login = async (req, res) => {
     if (!isPasswordCrt) {
       return res.status(400).json({ message: "Invalid credentials" });
     }
-    const token = jwt.sign({ email: existinguser.email, id: existinguser._id }, "test", {
+    const token = jwt.sign({ email: existinguser.email, id: existinguser._id }, process.env.JWT_SECRET, {
       expiresIn: "1h",
     });
     res.status(200).json({ result: existinguser, token });
